bc347004347f23b229297618482555649553501c
monanorremo <mona.norremo@gmail.com>
2021-10-05 09:27:18 +0000

    Troubleshooting javascript II

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 /*==================== MENU SHOW AND HIDDEN ====================*/
-const navMenu = document.getElementById('nav-menu'), 
-      navToggle = document.getElementById('nav-toggle'), 
-      navClose = document.getElementById('nav-close'),
+const navMenu = document.getElementById("nav-menu"),
+      navToggle = document.getElementById("nav-toggle"),
+      navClose = document.getElementById("nav-close")
 
 
 /*===== MENU SHOW =====*/
